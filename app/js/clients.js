723387da2cf9771c2f6c5e8dc353a5ca5a4bc1ed
Maria <m1a2r3.kovalenko@gmail.com>
2022-07-01 17:48:22 +0300

    добавлен ховер подчеркиваний ссылок, скролл секции активов

--- app/js/clients.js
+++ app/js/clients.js
@@ -45,3 +45,47 @@ clientsWrapper.onmousedown = function (event) {
     clientsWrapper.onmouseup = null;
   };
 };
+
+// const clientsWrapper = document.getElementById("clientsWrapper");
+// horizontalScroll(clientsWrapper);
+
+// const assetsScroll = document.getElementById("assetsScroll");
+// horizontalScroll(assetsScroll, [-30, 30], 15);
+
+// function horizontalScroll(element, border = [-100, 100], speed = 5) {
+//   element.style.touchAction = "none";
+//   let actualPosition =
+//     element.style.transform !== ""
+//       ? element.style.transform.match(/-\d+|\d+/)[0]
+//       : 0;
+//   let positionCursor;
+
+//   element.addEventListener("pointerdown", onScrollContainer);
+//   element.addEventListener("pointerup", StopScrollContainer);
+
+//   function onMouseMove(event) {
+//     let diff = (positionCursor - event.clientX) / speed;
+//     let percent = Math.floor(actualPosition - diff);
+//     element.style.transform = `translateX(${
+//       percent < border[0]
+//         ? border[0]
+//         : percent > border[1]
+//         ? border[1]
+//         : percent
+//     }%)`;
+//   }
+
+//   function onScrollContainer(event) {
+//     element.ondragstart = function () {
+//       return false;
+//     };
+//     positionCursor = event.clientX;
+//     element.addEventListener("pointermove", onMouseMove);
+//   }
+
+//   function StopScrollContainer(event) {
+//     actualPosition = element.style.transform.match(/-\d+|\d+/)[0];
+//     positionCursor = 0;
+//     element.removeEventListener("pointermove", onMouseMove);
+//   }
+// }
