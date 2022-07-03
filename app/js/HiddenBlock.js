d6690dac6735528005f5b7e47526e9c69d469565
Maria <m1a2r3.kovalenko@gmail.com>
2022-07-03 21:33:57 +0300

    ...

--- app/js/HiddenBlock.js
+++ app/js/HiddenBlock.js
@@ -1,4 +1,28 @@
-function hiddenBlock(
+function HoverHiddenBlock(
+  addClassName,
+  hoverButton,
+  showElement,
+  hoverElement = false
+) {
+  // hoverElement - показывать элемент при удержании указателя на нём
+
+  hoverButton.addEventListener("mouseover", Show);
+  hoverButton.addEventListener("mouseout", Hidden);
+
+  function Show() {
+    showElement.classList.toggle(addClassName);
+  }
+  function Hidden() {
+    showElement.classList.remove(addClassName);
+  }
+
+  if (hoverElement) {
+    showElement.addEventListener("mouseover", Show);
+    showElement.addEventListener("mouseout", Hidden);
+  }
+}
+
+function ClickHiddenBlock(
   addClassName,
   hoverButton,
   showElement,
@@ -24,8 +48,8 @@ function hiddenBlock(
 
 const buttonAboutUs = document.querySelector(".about-us__more-about-us");
 const advantages = document.querySelector(".advantages");
-hiddenBlock("show_advantages", buttonAboutUs, advantages, true);
+HoverHiddenBlock("show_advantages", buttonAboutUs, advantages, true);
 
 const buttonAdress = document.querySelector(".main-contacts__adress");
 const modal = document.querySelector(".modal");
-hiddenBlock("show_modal", buttonAdress, modal, true);
+HoverHiddenBlock("show_modal", buttonAdress, modal, true);
